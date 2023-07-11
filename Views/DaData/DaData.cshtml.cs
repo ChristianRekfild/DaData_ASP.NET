e324f85ca50f3c429fe71d6b9549cb5a053fc354
chrisrekfild <chrisrekfild@mail.ru>
2023-07-11 23:33:19 +0500

    Завтра уже буду что-то делать...

---
+++ Views/DaData/DaData.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DaDat_ASP.net.Views.DaData
+{
+    public class DaData : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
